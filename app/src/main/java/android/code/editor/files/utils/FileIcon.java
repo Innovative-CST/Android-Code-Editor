f96a4841062069c89cede64da4d6370ec8919ebb
TechnicalStudioDeveloper <appdeveloper400@gmail.com>
2023-05-22 21:52:14 +0530

    Added HTML icon from Pictogrammers to FileManagerActivity

--- app/src/main/java/android/code/editor/files/utils/FileIcon.java
+++ app/src/main/java/android/code/editor/files/utils/FileIcon.java
@@ -7,15 +7,17 @@ import android.widget.ImageView;
 import java.io.File;
 
 public class FileIcon {
-	public static void setUpIcon(Context context,String path,ImageView imageview) {
-		if(new File(path).isDirectory()) {
-			imageview.setImageResource(R.drawable.ic_folder_black_24dp);
-		} else if (new File(path).isFile()) {
-			if (FileManager.ifFileFormatIsEqualTo(path,"java")) {
-				imageview.setImageResource(R.drawable.ic_language_java);
-			} else {
-				imageview.setImageResource(0);
-			}
-		}
-	}
+    public static void setUpIcon(Context context, String path, ImageView imageview) {
+        if (new File(path).isDirectory()) {
+            imageview.setImageResource(R.drawable.ic_folder_black_24dp);
+        } else if (new File(path).isFile()) {
+            if (FileManager.ifFileFormatIsEqualTo(path, "java")) {
+                imageview.setImageResource(R.drawable.ic_language_java);
+            } else if (FileManager.ifFileFormatIsEqualTo(path, "html")) {
+                imageview.setImageResource(R.drawable.language_html);
+            } else {
+                imageview.setImageResource(0);
+            }
+        }
+    }
 }
