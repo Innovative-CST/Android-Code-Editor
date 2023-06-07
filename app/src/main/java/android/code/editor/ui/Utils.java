76204b57068da0c4a59742ef1686017a424b8207
TechnicalStudioDeveloper <appdeveloper400@gmail.com>
2023-06-07 12:57:28 +0530

    chore: Formatted code and removed unused imports in Utils

--- app/src/main/java/android/code/editor/ui/Utils.java
+++ app/src/main/java/android/code/editor/ui/Utils.java
@@ -2,7 +2,6 @@ package android.code.editor.ui;
 
 import android.app.Activity;
 import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.RippleDrawable;
 import android.os.Build;
@@ -11,18 +10,23 @@ import android.view.Window;
 import android.view.WindowManager;
 
 public class Utils {
-	public static void applyRippleEffect(View view,int focus,int ripple) {
-		GradientDrawable gradientDrawable = new GradientDrawable();
+    public static void applyRippleEffect(View view, int focus, int ripple) {
+        GradientDrawable gradientDrawable = new GradientDrawable();
         gradientDrawable.setColor(focus);
-        view.setBackground(new RippleDrawable(new ColorStateList(new int[][]{new int[0]}, new int[]{ripple}), gradientDrawable, null));
-	}
-	public static void StatusBarColor(final int _color,final Activity activity) {
-		//stutus bar
-		if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
-			Window w = activity.getWindow();
-			w.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-			w.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS); 
-			w.setStatusBarColor(_color);
-		}
-	}
+        view.setBackground(
+                new RippleDrawable(
+                        new ColorStateList(new int[][] {new int[0]}, new int[] {ripple}),
+                        gradientDrawable,
+                        null));
+    }
+
+    public static void StatusBarColor(final int _color, final Activity activity) {
+        // stutus bar
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
+            Window w = activity.getWindow();
+            w.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            w.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            w.setStatusBarColor(_color);
+        }
+    }
 }
