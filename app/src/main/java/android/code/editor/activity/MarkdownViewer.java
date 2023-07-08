7a7b81ba01906213ef375cf053e2658aa852a91f
TechnicalStudioDeveloper <appdeveloper400@gmail.com>
2023-07-08 19:28:22 +0530

    feat(feature): Added markdown viewer

---
+++ app/src/main/java/android/code/editor/activity/MarkdownViewer.java
@@ -0,0 +1,41 @@
+package android.code.editor.activity;
+
+import android.code.editor.R;
+import android.code.editor.ui.MaterialColorHelper;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import br.tiagohm.markdownview.MarkdownView;
+
+import java.io.File;
+
+public class MarkdownViewer extends AppCompatActivity {
+    
+    public MarkdownView markdown_view;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MaterialColorHelper.setUpTheme(this);
+        setContentView(R.layout.activity_markdown_viewer);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        toolbar.setNavigationOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View arg0) {
+                        onBackPressed();
+                    }
+                });
+        markdown_view = findViewById(R.id.markdown_view);
+        if (getIntent().getStringExtra("type").equals("url")) {
+            markdown_view.loadMarkdownFromUrl(getIntent().getStringExtra("data"));
+        } else if (getIntent().getStringExtra("type").equals("file")) {
+            markdown_view.loadMarkdownFromFile(new File(getIntent().getStringExtra("data")));
+        }
+    }
+}
