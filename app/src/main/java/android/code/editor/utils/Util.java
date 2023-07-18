0e6b2cff246d5bd99f2f18131d89696c7d8e4f2b
PHANTOM XOTIC <130784109+PHANTOMLOCAL@users.noreply.github.com>
2023-07-18 16:37:30 +0330

    Add files via upload

---
+++ app/src/main/java/android/code/editor/utils/Util.java
@@ -0,0 +1,37 @@
+package android.code.editor.utils;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.util.TypedValue;
+
+public class Utils {
+
+  private static Context mContext;
+
+  public static void init(Context context) {
+    mContext = context;
+  }
+
+  public static Context getContext() {
+    return mContext;
+  }
+
+  public static int pxToDp(int value) {
+    return (int)
+        TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP, value, mContext.getResources().getDisplayMetrics());
+  }
+
+  public static void copyText(final CharSequence text) {
+    ClipboardManager cm = (ClipboardManager) mContext.getSystemService(Context.CLIPBOARD_SERVICE);
+    cm.setPrimaryClip(ClipData.newPlainText(mContext.getPackageName(), text));
+  }
+
+  public static boolean isDarkMode() {
+    int uiMode =
+        mContext.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+    return uiMode == Configuration.UI_MODE_NIGHT_YES;
+  }
+}
