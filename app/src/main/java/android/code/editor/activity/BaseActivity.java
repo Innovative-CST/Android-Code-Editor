da5aa0947f5058fc4670f195ab9a322e2184074c
DevKumar00001 <appdeveloper400@gmail.com>
2023-07-23 09:24:50 +0530

    refactor: Formatted BaseActivity.java with google java formatter

--- app/src/main/java/android/code/editor/activity/BaseActivity.java
+++ app/src/main/java/android/code/editor/activity/BaseActivity.java
@@ -1,61 +1,56 @@
 package android.code.editor.activity;
 
 import android.code.editor.ui.MaterialColorHelper;
-import android.content.res.Resources;
 import android.os.Bundle;
-
-import android.util.TypedValue;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-import com.google.android.material.R;
 import com.google.android.material.color.DynamicColors;
 
 public class BaseActivity extends AppCompatActivity {
-    public int theme;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (!DynamicColors.isDynamicColorAvailable()) {
-            theme = MaterialColorHelper.getCurrentTheme(this);
-            MaterialColorHelper.setUpTheme(this);
-        }
-        refreshThemeStatusIfRequired();
-    }
+  public int theme;
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        refreshThemeColorStatusIfRequired();
-        refreshThemeStatusIfRequired();
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    if (!DynamicColors.isDynamicColorAvailable()) {
+      theme = MaterialColorHelper.getCurrentTheme(this);
+      MaterialColorHelper.setUpTheme(this);
     }
+    refreshThemeStatusIfRequired();
+  }
 
-    public void refreshThemeColorStatusIfRequired() {
-        if (!DynamicColors.isDynamicColorAvailable()) {
-            if (MaterialColorHelper.getCurrentTheme(this) != theme) {
-                recreate();
-            }
-        }
-    }
+  @Override
+  protected void onResume() {
+    super.onResume();
+    refreshThemeColorStatusIfRequired();
+    refreshThemeStatusIfRequired();
+  }
 
-    public void refreshThemeStatusIfRequired() {
-        switch (SettingActivity.getThemeTypeInInt(this)) {
-            case 0:
-                if (AppCompatDelegate.getDefaultNightMode() != AppCompatDelegate.MODE_NIGHT_NO) {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                }
-                break;
-            case 1:
-                if (AppCompatDelegate.getDefaultNightMode() != AppCompatDelegate.MODE_NIGHT_YES) {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                }
-                break;
-            case 2:
-                if (AppCompatDelegate.getDefaultNightMode()
-                        != AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM) {
-                    AppCompatDelegate.setDefaultNightMode(
-                            AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
-                }
-                break;
-        }
+  public void refreshThemeColorStatusIfRequired() {
+    if (!DynamicColors.isDynamicColorAvailable()) {
+      if (MaterialColorHelper.getCurrentTheme(this) != theme) {
+        recreate();
+      }
     }
+  }
+
+  public void refreshThemeStatusIfRequired() {
+    switch (SettingActivity.getThemeTypeInInt(this)) {
+      case 0:
+        if (AppCompatDelegate.getDefaultNightMode() != AppCompatDelegate.MODE_NIGHT_NO) {
+          AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+        break;
+      case 1:
+        if (AppCompatDelegate.getDefaultNightMode() != AppCompatDelegate.MODE_NIGHT_YES) {
+          AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        break;
+      case 2:
+        if (AppCompatDelegate.getDefaultNightMode() != AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM) {
+          AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        }
+        break;
+    }
+  }
 }
