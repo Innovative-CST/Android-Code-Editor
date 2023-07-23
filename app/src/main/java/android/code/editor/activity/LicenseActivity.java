ecb52d961ada2b5c043dfb8db18e8ba7426fc8da
DevKumar00001 <appdeveloper400@gmail.com>
2023-07-23 09:29:50 +0530

    refactor: Formatted LicenseActivity.java with google java formatter

--- app/src/main/java/android/code/editor/activity/LicenseActivity.java
+++ app/src/main/java/android/code/editor/activity/LicenseActivity.java
@@ -7,9 +7,7 @@ import android.text.method.LinkMovementMethod;
 import android.text.util.Linkify;
 import android.view.View;
 import android.widget.TextView;
-
 import androidx.appcompat.widget.Toolbar;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,48 +15,48 @@ import java.io.InputStreamReader;
 
 public class LicenseActivity extends BaseActivity {
 
-    private TextView licenseText;
+  private TextView licenseText;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_license);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("Open Source Licenses");
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        toolbar.setNavigationOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View arg0) {
-                        // TODO: Implement this method
-                        onBackPressed();
-                    }
-                });
-        licenseText = findViewById(R.id.licenseText);
-        licenseText.setAutoLinkMask(Linkify.WEB_URLS);
-        licenseText.setMovementMethod(LinkMovementMethod.getInstance());
-        licenseText.setText(readFileFromAssets(getAssets(), "oos.text"));
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_license);
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    toolbar.setTitle("Open Source Licenses");
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    getSupportActionBar().setHomeButtonEnabled(true);
+    toolbar.setNavigationOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View arg0) {
+            // TODO: Implement this method
+            onBackPressed();
+          }
+        });
+    licenseText = findViewById(R.id.licenseText);
+    licenseText.setAutoLinkMask(Linkify.WEB_URLS);
+    licenseText.setMovementMethod(LinkMovementMethod.getInstance());
+    licenseText.setText(readFileFromAssets(getAssets(), "oos.text"));
+  }
+
+  public String readFileFromAssets(AssetManager assetManager, String fileName) {
+    StringBuilder stringBuilder = new StringBuilder();
+    try {
+      InputStream inputStream = assetManager.open(fileName);
+      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+      String line;
+      while ((line = bufferedReader.readLine()) != null) {
+        stringBuilder.append(line);
+        stringBuilder.append("\n");
+      }
+
+      bufferedReader.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
 
-    public String readFileFromAssets(AssetManager assetManager, String fileName) {
-        StringBuilder stringBuilder = new StringBuilder();
-        try {
-            InputStream inputStream = assetManager.open(fileName);
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line);
-                stringBuilder.append("\n");
-            }
-
-            bufferedReader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return stringBuilder.toString();
-    }
+    return stringBuilder.toString();
+  }
 }
