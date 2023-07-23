c36a659f9a327c60d2b42159ef7bb63106b456eb
DevKumar00001 <appdeveloper400@gmail.com>
2023-07-23 09:47:31 +0530

    refactor: Formatted LanguageModeHandler.java with google java formatter

--- app/src/main/java/android/code/editor/utils/LanguageModeHandler.java
+++ app/src/main/java/android/code/editor/utils/LanguageModeHandler.java
@@ -3,19 +3,19 @@ package android.code.editor.utils;
 import editor.tsd.tools.Language;
 
 public class LanguageModeHandler {
-    public static String getLanguageModeForExtension(String ext) {
-        if (ext.equals("java")) {
-            return Language.Java;
-        } else if (ext.equals("xml")) {
-            return Language.XML;
-        } else if (ext.equals("html")) {
-            return Language.HTML;
-        } else if (ext.equals("css")) {
-            return Language.CSS;
-        } else if (ext.equals("md")) {
-            return Language.JavaScript;
-        } else {
-            return Language.Markdown;
-        }
+  public static String getLanguageModeForExtension(String ext) {
+    if (ext.equals("java")) {
+      return Language.Java;
+    } else if (ext.equals("xml")) {
+      return Language.XML;
+    } else if (ext.equals("html")) {
+      return Language.HTML;
+    } else if (ext.equals("css")) {
+      return Language.CSS;
+    } else if (ext.equals("md")) {
+      return Language.JavaScript;
+    } else {
+      return Language.Markdown;
     }
+  }
 }
