5ea32ca4a95afa769b3c7f2c3b3783c711d66c8b
DevKumar00001 <appdeveloper400@gmail.com>
2023-07-23 10:05:12 +0530

    refactor: Formatted Editor.java with google java formatter

--- editor/src/main/java/editor/tsd/editors/Editor.java
+++ editor/src/main/java/editor/tsd/editors/Editor.java
@@ -1,19 +1,17 @@
 package editor.tsd.editors;
 
-import io.github.rosemoe.sora.widget.CodeEditor;
-
 public interface Editor {
-    public void setCode(String Code);
+  public void setCode(String Code);
 
-    public String getCode();
+  public String getCode();
 
-    public int getCodeEditorType();
+  public int getCodeEditorType();
 
-    public void setLanguageMode(String LqnguageMode);
+  public void setLanguageMode(String LqnguageMode);
 
-    public void setTheme(String theme);
+  public void setTheme(String theme);
 
-    public void moveCursorHorizontally(int steps);
+  public void moveCursorHorizontally(int steps);
 
-    public void moveCursorVertically(int steps);
+  public void moveCursorVertically(int steps);
 }
