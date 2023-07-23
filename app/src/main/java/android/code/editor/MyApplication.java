43ae74290de0d5497dc278ca8ccdfdea5e99b1b1
DevKumar00001 <appdeveloper400@gmail.com>
2023-07-23 09:49:49 +0530

    refactor: Formatted MyApplication.java with google java formatter

--- app/src/main/java/android/code/editor/MyApplication.java
+++ app/src/main/java/android/code/editor/MyApplication.java
@@ -1,60 +1,52 @@
 package android.code.editor;
 
-import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.Application;
 import android.app.PendingIntent;
 import android.code.editor.activity.DebugActivity;
-import android.code.editor.activity.SettingActivity;
-import android.code.editor.utils.Setting;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.os.Process;
 import android.util.Log;
-import androidx.appcompat.app.AppCompatDelegate;
 import com.google.android.material.color.DynamicColors;
 
 public class MyApplication extends Application {
-    private static Context mApplicationContext;
-    private Thread.UncaughtExceptionHandler uncaughtExceptionHandler;
+  private static Context mApplicationContext;
+  private Thread.UncaughtExceptionHandler uncaughtExceptionHandler;
 
-    public static Context getContext() {
-        return mApplicationContext;
-    }
+  public static Context getContext() {
+    return mApplicationContext;
+  }
 
-    @Override
-    public void onCreate() {
+  @Override
+  public void onCreate() {
 
-        mApplicationContext = getApplicationContext();
+    mApplicationContext = getApplicationContext();
 
-        DynamicColors.applyToActivitiesIfAvailable(this);
+    DynamicColors.applyToActivitiesIfAvailable(this);
 
-        this.uncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+    this.uncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
 
-        Thread.setDefaultUncaughtExceptionHandler(
-                new Thread.UncaughtExceptionHandler() {
-                    @Override
-                    public void uncaughtException(Thread thread, Throwable throwable) {
-                        Intent intent = new Intent(getApplicationContext(), DebugActivity.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                        intent.putExtra("error", Log.getStackTraceString(throwable));
-                        PendingIntent pendingIntent =
-                                PendingIntent.getActivity(
-                                        getApplicationContext(),
-                                        11111,
-                                        intent,
-                                        PendingIntent.FLAG_ONE_SHOT);
+    Thread.setDefaultUncaughtExceptionHandler(
+        new Thread.UncaughtExceptionHandler() {
+          @Override
+          public void uncaughtException(Thread thread, Throwable throwable) {
+            Intent intent = new Intent(getApplicationContext(), DebugActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            intent.putExtra("error", Log.getStackTraceString(throwable));
+            PendingIntent pendingIntent =
+                PendingIntent.getActivity(
+                    getApplicationContext(), 11111, intent, PendingIntent.FLAG_ONE_SHOT);
 
-                        AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-                        am.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, 1000, pendingIntent);
+            AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+            am.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, 1000, pendingIntent);
 
-                        Process.killProcess(Process.myPid());
-                        System.exit(1);
+            Process.killProcess(Process.myPid());
+            System.exit(1);
 
-                        uncaughtExceptionHandler.uncaughtException(thread, throwable);
-                    }
-                });
-        super.onCreate();
-    }
+            uncaughtExceptionHandler.uncaughtException(thread, throwable);
+          }
+        });
+    super.onCreate();
+  }
 }
