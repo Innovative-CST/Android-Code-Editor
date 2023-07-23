cb40b42c6a5f567f698d2cb891649297bb24b98f
DevKumar00001 <appdeveloper400@gmail.com>
2023-07-23 10:08:42 +0530

    refactor: Formatted CodeEditorLayout.java with google java formatter

--- editor/src/main/java/editor/tsd/widget/CodeEditorLayout.java
+++ editor/src/main/java/editor/tsd/widget/CodeEditorLayout.java
@@ -5,174 +5,171 @@ import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.webkit.WebView;
 import android.widget.LinearLayout;
-
 import editor.tsd.R;
 import editor.tsd.editors.AceEditor;
 import editor.tsd.editors.Editor;
 import editor.tsd.editors.SoraEditor;
-
 import io.github.rosemoe.sora.widget.CodeEditor;
 
 public class CodeEditorLayout extends LinearLayout {
-    public static final int AceCodeEditor = 0;
-    public static final int SoraCodeEditor = 1;
+  public static final int AceCodeEditor = 0;
+  public static final int SoraCodeEditor = 1;
 
-    public AceEditor aceEditor;
-    public SoraEditor soraEditor;
-    public int editor;
-    public String code;
-    public Context conText;
-    public Editor CurrentCodeEditor;
+  public AceEditor aceEditor;
+  public SoraEditor soraEditor;
+  public int editor;
+  public String code;
+  public Context conText;
+  public Editor CurrentCodeEditor;
 
-    public CodeEditorLayout(Context context) {
-        super(context);
-        this.conText = context;
-        try {
-            this.editor = AceCodeEditor;
-            this.code = "";
-        } finally {
-            init();
-        }
+  public CodeEditorLayout(Context context) {
+    super(context);
+    this.conText = context;
+    try {
+      this.editor = AceCodeEditor;
+      this.code = "";
+    } finally {
+      init();
     }
+  }
 
-    public CodeEditorLayout(Context context, int editor) {
-        super(context);
-        this.conText = context;
-        try {
-            this.editor = editor;
-            this.code = "";
-        } finally {
-            init();
-        }
+  public CodeEditorLayout(Context context, int editor) {
+    super(context);
+    this.conText = context;
+    try {
+      this.editor = editor;
+      this.code = "";
+    } finally {
+      init();
     }
+  }
 
-    public CodeEditorLayout(Context context, String code) {
-        super(context);
-        this.conText = context;
-        try {
-            this.editor = AceCodeEditor;
-            this.code = code;
-        } finally {
-            init();
-        }
+  public CodeEditorLayout(Context context, String code) {
+    super(context);
+    this.conText = context;
+    try {
+      this.editor = AceCodeEditor;
+      this.code = code;
+    } finally {
+      init();
     }
+  }
 
-    public CodeEditorLayout(Context context, int editor, String code) {
-        super(context);
-        this.conText = context;
-        try {
-            this.editor = editor;
-            this.code = code;
-        } finally {
-            init();
-        }
+  public CodeEditorLayout(Context context, int editor, String code) {
+    super(context);
+    this.conText = context;
+    try {
+      this.editor = editor;
+      this.code = code;
+    } finally {
+      init();
     }
+  }
 
-    public CodeEditorLayout(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        conText = context;
-        TypedArray a =
-                context.getTheme()
-                        .obtainStyledAttributes(attrs, R.styleable.CodeEditorLayout, 0, 0);
-        try {
-            editor = a.getInt(R.styleable.CodeEditorLayout_codeEditor, 0);
-            code = a.getString(R.styleable.CodeEditorLayout_setCode);
-        } finally {
-            // Set Code Editor as SoraEditor
-            init();
-            a.recycle();
-        }
+  public CodeEditorLayout(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    conText = context;
+    TypedArray a =
+        context.getTheme().obtainStyledAttributes(attrs, R.styleable.CodeEditorLayout, 0, 0);
+    try {
+      editor = a.getInt(R.styleable.CodeEditorLayout_codeEditor, 0);
+      code = a.getString(R.styleable.CodeEditorLayout_setCode);
+    } finally {
+      // Set Code Editor as SoraEditor
+      init();
+      a.recycle();
     }
+  }
 
-    private void init() {
-        // Set Code Editor as SoraEditor
-        if (editor == SoraCodeEditor) {
-            soraEditor = new SoraEditor(conText);
-            CurrentCodeEditor = soraEditor;
-            soraEditor.setCode(code);
-            addView(soraEditor.getCodeEditor());
-        } else if (editor == AceCodeEditor) {
-            aceEditor = new AceEditor(conText);
-            CurrentCodeEditor = aceEditor;
-            addView(aceEditor.getCodeEditor());
-            aceEditor.setCode(code);
-        }
+  private void init() {
+    // Set Code Editor as SoraEditor
+    if (editor == SoraCodeEditor) {
+      soraEditor = new SoraEditor(conText);
+      CurrentCodeEditor = soraEditor;
+      soraEditor.setCode(code);
+      addView(soraEditor.getCodeEditor());
+    } else if (editor == AceCodeEditor) {
+      aceEditor = new AceEditor(conText);
+      CurrentCodeEditor = aceEditor;
+      addView(aceEditor.getCodeEditor());
+      aceEditor.setCode(code);
     }
+  }
 
-    public void setCode(String newCode) {
-        code = newCode;
-        CurrentCodeEditor.setCode(newCode);
-    }
+  public void setCode(String newCode) {
+    code = newCode;
+    CurrentCodeEditor.setCode(newCode);
+  }
 
-    public String getCode() {
-        if (CurrentCodeEditor != null) {
-            return CurrentCodeEditor.getCode();
-        } else {
-            return "";
-        }
+  public String getCode() {
+    if (CurrentCodeEditor != null) {
+      return CurrentCodeEditor.getCode();
+    } else {
+      return "";
     }
+  }
 
-    public void setEditor(int newEditor) {
-        if (newEditor != editor) {
-            if (CodeEditorLayout.AceCodeEditor == newEditor) {
-                aceEditor = new AceEditor(conText);
-                CurrentCodeEditor = aceEditor;
-                aceEditor.setCode(getCode());
-                removeView(soraEditor.getCodeEditor());
-                addView(aceEditor.getCodeEditor());
-                editor = AceCodeEditor;
-                soraEditor = null;
-                requestLayout();
-            } else if (CodeEditorLayout.SoraCodeEditor == newEditor) {
-                soraEditor = new SoraEditor(conText);
-                CurrentCodeEditor = soraEditor;
-                soraEditor.setCode(getCode());
-                removeView(aceEditor.getCodeEditor());
-                addView(soraEditor.getCodeEditor());
-                editor = SoraCodeEditor;
-                aceEditor = null;
-                requestLayout();
-            }
-        }
+  public void setEditor(int newEditor) {
+    if (newEditor != editor) {
+      if (CodeEditorLayout.AceCodeEditor == newEditor) {
+        aceEditor = new AceEditor(conText);
+        CurrentCodeEditor = aceEditor;
+        aceEditor.setCode(getCode());
+        removeView(soraEditor.getCodeEditor());
+        addView(aceEditor.getCodeEditor());
+        editor = AceCodeEditor;
+        soraEditor = null;
+        requestLayout();
+      } else if (CodeEditorLayout.SoraCodeEditor == newEditor) {
+        soraEditor = new SoraEditor(conText);
+        CurrentCodeEditor = soraEditor;
+        soraEditor.setCode(getCode());
+        removeView(aceEditor.getCodeEditor());
+        addView(soraEditor.getCodeEditor());
+        editor = SoraCodeEditor;
+        aceEditor = null;
+        requestLayout();
+      }
     }
+  }
 
-    public void setLanguageMode(String LanguageMode) {
-        CurrentCodeEditor.setLanguageMode(LanguageMode);
-    }
+  public void setLanguageMode(String LanguageMode) {
+    CurrentCodeEditor.setLanguageMode(LanguageMode);
+  }
 
-    public void setTheme(String theme) {
-        CurrentCodeEditor.setTheme(theme);
-    }
+  public void setTheme(String theme) {
+    CurrentCodeEditor.setTheme(theme);
+  }
 
-    public Editor getEditor() {
-        return CurrentCodeEditor;
-    }
+  public Editor getEditor() {
+    return CurrentCodeEditor;
+  }
 
-    public int getCurrentEditorType() {
-        return editor;
-    }
+  public int getCurrentEditorType() {
+    return editor;
+  }
 
-    public CodeEditor getSoraCodeEditor() {
-        return soraEditor.getCodeEditor();
-    }
+  public CodeEditor getSoraCodeEditor() {
+    return soraEditor.getCodeEditor();
+  }
 
-    public SoraEditor getSoraEditor() {
-        return soraEditor;
-    }
+  public SoraEditor getSoraEditor() {
+    return soraEditor;
+  }
 
-    public WebView getAceCodeEditor() {
-        return aceEditor.getCodeEditor();
-    }
+  public WebView getAceCodeEditor() {
+    return aceEditor.getCodeEditor();
+  }
 
-    public AceEditor getAceEditor() {
-        return aceEditor;
-    }
+  public AceEditor getAceEditor() {
+    return aceEditor;
+  }
 
-    public void moveCursorHorizontally(int steps) {
-        CurrentCodeEditor.moveCursorHorizontally(steps);
-    }
+  public void moveCursorHorizontally(int steps) {
+    CurrentCodeEditor.moveCursorHorizontally(steps);
+  }
 
-    public void moveCursorVertically(int steps) {
-        CurrentCodeEditor.moveCursorVertically(steps);
-    }
+  public void moveCursorVertically(int steps) {
+    CurrentCodeEditor.moveCursorVertically(steps);
+  }
 }
