63b8a7df2b8165c16d74dfba49a5113c8af189df
DevKumar00001 <appdeveloper400@gmail.com>
2023-07-23 09:40:33 +0530

    refactor: Formatted MaterialColorHelper.java with google java formatter

--- app/src/main/java/android/code/editor/ui/MaterialColorHelper.java
+++ app/src/main/java/android/code/editor/ui/MaterialColorHelper.java
@@ -5,34 +5,34 @@ import android.code.editor.utils.Setting;
 import android.content.Context;
 
 public class MaterialColorHelper {
-    public static final String AppTheme = "AppTheme";
-    public static final String AppTheme2 = "AppTheme2";
-    public static final String AppTheme3 = "AppTheme3";
-    public static final String AppTheme4 = "AppTheme4";
+  public static final String AppTheme = "AppTheme";
+  public static final String AppTheme2 = "AppTheme2";
+  public static final String AppTheme3 = "AppTheme3";
+  public static final String AppTheme4 = "AppTheme4";
 
-    public static void setUpTheme(Context context) {
-        context.setTheme(getCurrentTheme(context));
-    }
+  public static void setUpTheme(Context context) {
+    context.setTheme(getCurrentTheme(context));
+  }
 
-    public static int getCurrentTheme(Context context) {
-        int theme;
-        switch (Setting.SaveInFile.getSettingString(
-                Setting.Key.Theme, Setting.Default.Theme, context)) {
-            case AppTheme:
-                theme = R.style.AppTheme;
-                break;
-            case AppTheme2:
-                theme = R.style.AppTheme2;
-                break;
-            case AppTheme3:
-                theme = R.style.AppTheme3;
-                break;
-            case AppTheme4:
-                theme = R.style.AppTheme4;
-                break;
-            default:
-                return R.style.AppTheme;
-        }
-        return theme;
+  public static int getCurrentTheme(Context context) {
+    int theme;
+    switch (Setting.SaveInFile.getSettingString(
+        Setting.Key.Theme, Setting.Default.Theme, context)) {
+      case AppTheme:
+        theme = R.style.AppTheme;
+        break;
+      case AppTheme2:
+        theme = R.style.AppTheme2;
+        break;
+      case AppTheme3:
+        theme = R.style.AppTheme3;
+        break;
+      case AppTheme4:
+        theme = R.style.AppTheme4;
+        break;
+      default:
+        return R.style.AppTheme;
     }
+    return theme;
+  }
 }
