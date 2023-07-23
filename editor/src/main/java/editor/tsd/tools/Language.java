4c3e45da879879538e50ff0853bad4670e5475c9
DevKumar00001 <appdeveloper400@gmail.com>
2023-07-23 10:07:06 +0530

    refactor: Formatted Language.java with google java formatter

--- editor/src/main/java/editor/tsd/tools/Language.java
+++ editor/src/main/java/editor/tsd/tools/Language.java
@@ -1,10 +1,10 @@
 package editor.tsd.tools;
 
 public class Language {
-    public static final String Java = "Java";
-    public static final String JavaScript = "JavaScript";
-    public static final String HTML = "HTML";
-    public static final String CSS = "CSS";
-    public static final String XML = "XML";
-    public static final String Markdown = "Markdown";
+  public static final String Java = "Java";
+  public static final String JavaScript = "JavaScript";
+  public static final String HTML = "HTML";
+  public static final String CSS = "CSS";
+  public static final String XML = "XML";
+  public static final String Markdown = "Markdown";
 }
