52ed7b02d845f12099e3318bc68d730b53fddcfb
CyberGlitch01 <kadev70@gmail.com>
2023-08-20 11:53:28 +0530

    chore: Use FileUtils#readFileFromAssets in TextMateProvider to prevent duplication

--- editor/src/main/java/editor/tsd/editors/sora/lang/textmate/provider/TextMateProvider.java
+++ editor/src/main/java/editor/tsd/editors/sora/lang/textmate/provider/TextMateProvider.java
@@ -1,14 +1,10 @@
 package editor.tsd.editors.sora.lang.textmate.provider;
 
+import android.code.editor.common.utils.FileUtils;
 import android.content.Context;
-import android.content.res.AssetManager;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import io.github.rosemoe.sora.langs.textmate.registry.GrammarRegistry;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Map;
 
 public class TextMateProvider {
@@ -22,28 +18,8 @@ public class TextMateProvider {
     scopes =
         new Gson()
             .fromJson(
-                readFileFromAssets(context.getAssets(), "Editor/SoraEditor/language_scopes.json"),
+                FileUtils.readFileFromAssets(context.getAssets(), "Editor/SoraEditor/language_scopes.json"),
                 type);
     return scopes.get(fileExt);
-  }
-
-  public static String readFileFromAssets(AssetManager assetManager, String fileName) {
-    StringBuilder stringBuilder = new StringBuilder();
-    try {
-      InputStream inputStream = assetManager.open(fileName);
-      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-
-      String line;
-      while ((line = bufferedReader.readLine()) != null) {
-        stringBuilder.append(line);
-        stringBuilder.append("\n");
-      }
-
-      bufferedReader.close();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-
-    return stringBuilder.toString();
   }
 }
