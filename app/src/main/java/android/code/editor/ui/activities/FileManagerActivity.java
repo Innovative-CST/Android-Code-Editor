cc58a3ca46b8ece4d2513b352e666b87677b7f2c
DevKumar00001 <appdeveloper400@gmail.com>
2023-09-17 12:31:28 +0530

    fix: Fix license text

--- app/src/main/java/android/code/editor/ui/activities/FileManagerActivity.java
+++ app/src/main/java/android/code/editor/ui/activities/FileManagerActivity.java
@@ -12,7 +12,7 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *   along with AndroidIDE.  If not, see <https://www.gnu.org/licenses/>.
+ *   along with Android Code Editor.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package android.code.editor.ui.activities;
